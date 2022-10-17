4a56eb585e99934db8dee69939d95630d6ebc9ea
lukasbicus <ing.lukas.bicus@gmail.com>
2022-10-17 10:42:04 +0200

    Add AdvancedExample with fetchCamera hook

--- src/AdvancedExample.tsx
+++ src/AdvancedExample.tsx
@@ -1,9 +1,35 @@
+import {Html5QrcodeSupportedFormats} from "html5-qrcode";
+import {Html5QrcodeScanType} from "html5-qrcode/esm/core";
 import React from 'react'
+import {useFetchCameras} from "./useFetchCameras";
+
+const CONFIG = {
+  fps: 4,
+  qrbox: {width: 300, height: 200},
+  formatsToSupport: [
+    Html5QrcodeSupportedFormats.CODE_128,
+    Html5QrcodeSupportedFormats.QR_CODE
+  ],
+  rememberLastUsedCamera: true,
+  supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
+}
 
 export const AdvancedExample: React.FC = () => {
-  return (
-    <div>
-      Advanced example
+  const {state: {loading, error, cameraDevices}} = useFetchCameras()
+  if (loading) {
+    return <div>
+      <h6>
+        Detecting available cameras
+      </h6>
     </div>
+  }
+  if (error) {
+    return (<div><h6>Failed to detect cameras</h6></div>)
+  }
+  if (cameraDevices.length === 0) {
+    return <div><h6>No available cameras</h6></div>
+  }
+  return (
+    <div>Camera id: {cameraDevices[0].id}</div>
   )
 }
---
+++ src/useFetchCameras.tsx
@@ -0,0 +1,40 @@
+import {Html5Qrcode} from 'html5-qrcode'
+import {CameraDevice} from 'html5-qrcode/esm/core'
+import {useCallback, useEffect, useState} from 'react'
+
+interface IFetchCameras {
+  loading: boolean
+  error?: Error
+  cameraDevices: CameraDevice[]
+}
+const defaultState: IFetchCameras = {
+  loading: false,
+  cameraDevices: []
+}
+
+export const useFetchCameras = () => {
+  const [state, setState] = useState<IFetchCameras>(defaultState)
+  const fetchCameras = useCallback(async () => {
+    try {
+      if (!state.loading) {
+        setState((prevState) => ({...prevState, loading: true}))
+        const result = await Html5Qrcode.getCameras()
+        setState({
+          loading: false,
+          cameraDevices: result
+        })
+      }
+    } catch (error) {
+      setState({
+        loading: false,
+        error: new Error('Not permitted'),
+        cameraDevices: []
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchCameras()
+  }, [fetchCameras])
+  return {state, refetchCameras: fetchCameras}
+}
